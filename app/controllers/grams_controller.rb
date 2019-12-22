bc38d912c532eafc08e382c10119318d04404d09
Jordan Roszkowiak <jordanroszkowiak@gmail.com>
2019-12-22 23:45:24 +0000

    Grammable 12ish

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,32 @@
+class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  def new
+    @gram = Gram.new
+  end
+
+
+  def index
+  end
+
+ def show
+  @gram = Gram.find_by_id(params[:id])
+  if @gram.blank?
+    render plain: 'Not Found :(', status: :not_found
+  end
+end
+
+  def create
+  @gram = current_user.grams.create(gram_params)
+  if @gram.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+end
